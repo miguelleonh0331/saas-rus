bb80a798c8b9d98162c8ced2a240471c41336619
Diego <diego@local.dev>
2026-06-12 15:46:22 +0200

    Add Capacitor Android project

---
+++ frontend/android/app/src/main/java/com/prosegin/micajarus/MainActivity.java
@@ -0,0 +1,5 @@
+package com.prosegin.micajarus;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.prosegin.micajarus',
+  appName: 'Mi Caja RUS',
+  webDir: 'dist'
+};
+
+export default config;
